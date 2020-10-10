48ec34a734973f5293ccf4664bbb8c5b0c134e1b
kaprichioza <nnicead@gmail.com>
2020-10-10 20:04:52 +0300

    form loyaut

--- pages/components/contact-form/contact-form.jsx
+++ pages/components/contact-form/contact-form.jsx
@@ -1,16 +1,41 @@
 import React from 'react'
 import styles from './contact-form.module.css';
-export const ContactForm = () => {
+
+export const ContactForm = (props) => {
+
     return (
         <div className={styles.formWrapper}>
-            <div className={styles.email}>
-                <div className={styles.emailIco}></div>
-                <div className={styles.emailText}>Ivanova@mail.ru</div>
-            </div>
-            <div className={styles.phoneNumber}>
-                <div className={styles.phoneIco}></div>
-                <div className={styles.phoneNumberText}>Укажите номер телефона</div>
-            </div>
+            {props.isEditable ?
+                <form className={styles.form}>
+                    <div className={styles.fields}>
+                        <div className={styles.field}>
+                            <div>FIO</div>
+                            <div></div>
+                        </div>
+                        <div>
+                            <div>Phone</div>
+                            <div></div>
+                        </div>
+                        <div>
+                            <div>EMAIL</div>
+                            <div></div>
+                        </div>
+                    </div>
+                    <div className={styles.button}>
+                        <button>button</button>
+                    </div>
+                </form>
+
+                : <>
+                    <div className={styles.email}>
+                        <div className={styles.emailIco}></div>
+                        <div className={styles.emailText}>Ivanova@mail.ru</div>
+                    </div>
+                    <div className={styles.phoneNumber}>
+                        <div className={styles.phoneIco}></div>
+                        <div className={styles.phoneNumberText}>Укажите номер телефона</div>
+                    </div>
+                </>}
         </div>
     )
 }
--- pages/components/main/main.jsx
+++ pages/components/main/main.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './main.module.css';
 import { ContactForm } from './../contact-form/contact-form';
+import cn from 'classnames';
 
-
+const initialState = {
+    name: '',
+    email: '',
+    phone: '',
+    isEditEnabled: false,
+};
 export const Main = (props) => {
+    const [contactFormState, setContactFormState] = useState(initialState);
+    const { isEditEnabled } = contactFormState;
+    const toggleEdit = () => {
+        setContactFormState({ ...contactFormState, isEditEnabled: !isEditEnabled });
+    }
     return (
         <div className={styles.wrapper}>
             <div className={styles.info}>
@@ -13,12 +24,12 @@ export const Main = (props) => {
                 </div>
                 <div className={styles.rightSide}>
                     <div className={styles.edit}>
-                        <div className={styles.editText}>РЕДАКТИРОВАТЬ</div>
-                        <div className={styles.editIco}></div>
+                        <div className={styles.editText} onClick={toggleEdit}>{isEditEnabled ? 'ЗАКРЫТЬ' : 'РЕДАКТИРОВАТЬ'}</div>
+                        <div className={ isEditEnabled ? styles.crossIco : styles.editIco }></div>
                     </div>
                 </div>
             </div>
-            <ContactForm />
-        </div>
+            <ContactForm isEditable={isEditEnabled}/>
+        </div >
     )
 }
