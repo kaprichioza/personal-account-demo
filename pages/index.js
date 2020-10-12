b0aa62292d53ee77496bc50f5936273cb7aeb811
kaprichioza <nnicead@gmail.com>
2020-10-12 14:52:39 +0300

    refactor

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,16 @@
 import React , { useState } from 'react';
 import Head from 'next/head';
-import { Page } from './components/Page/page'
-import { Header } from './components/header/header';
-import { Navigation } from './components/navigation/navigation';
+import { Page } from './components/Page/page';
 import { Main } from './components/main/main';
 
-let shortName = 'Иванова А';
-export default function Home() {
-  const [nameState, setNameState] = useState(shortName);
-  const name = (name) => {
-    shortName = name.match(/^(\s*.*?\s).*\w{0,1}/)[1] + name.split(' ')[1].match(/^\W/);
-    setNameState({ shortName: shortName });
-  };
+export default function Home() {  
   return (
     <>
       <Head>
         <title>personal account demo</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Page>
-        <Header>{nameState.shortName}</Header>
-        <Navigation />
+      <Page>        
         <Main name={name} />
       </Page>
     </>
