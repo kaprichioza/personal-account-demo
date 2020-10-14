baadd09979577faa755d16384e735b391a74deb3
kaprichioza <nnicead@gmail.com>
2020-10-14 19:55:18 +0300

    fix

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Page>        
-        <Main name={name} />
+        <Main />
       </Page>
     </>
   )
